b1e0e6121c2467d50e9da5cfe1d60f05cd37d751
tim <timwongbrooklyn@gmail.com>
2017-06-04 23:30:54 -0400

    add mongo dep

--- src/main/java/com/askfolio/test/MyFirstVerticle.java
+++ src/main/java/com/askfolio/test/MyFirstVerticle.java
@@ -4,6 +4,7 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
 
 public class MyFirstVerticle extends AbstractVerticle {
 
@@ -19,6 +20,9 @@ public class MyFirstVerticle extends AbstractVerticle {
                     .putHeader("content-type", "text/html")
                     .end("<h1>Hello from my first Vert.x 3 application</h1>");
         });
+
+        // Serve static resources from the /assets directory
+        router.route("/assets/*").handler(StaticHandler.create("assets"));
 
         // Create the HTTP server and pass the "accept" method to the request handler.
         vertx
---
+++ src/main/java/com/askfolio/user/User.java
@@ -0,0 +1,80 @@
+package com.askfolio.user;
+
+import com.google.common.base.Objects;
+
+public class User {
+
+    private String userName;
+    private String passWord;
+    private String firstName;
+    private String lastName;
+    private String address;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equal(userName, user.userName) &&
+                Objects.equal(passWord, user.passWord) &&
+                Objects.equal(firstName, user.firstName) &&
+                Objects.equal(lastName, user.lastName) &&
+                Objects.equal(address, user.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(userName, passWord, firstName, lastName, address);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", passWord='" + passWord + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
